edc1f7832bf9aac2738bf294fe7042ba893df3fe
yedohkang913 <ykang@stuy.edu>
2017-04-21 08:33:08 -0400

    Update Ticket.java

--- Ticket.java
+++ Ticket.java
@@ -3,13 +3,15 @@ public class Ticket implements Comparable{
     public Ticket(){
 	priority = 1;
     }
-    public compareTo(Ticket other){
+    public int compareTo(Ticket other){
 	if( this.priority < other.priority){
 	    return -1;
 	}
 	else if(this.priority == other.priority){
 	    return 0;
 	}
-	else{return 1;}
+	else{
+		return 1;
+	}
     }
 }
