53ccb28a2ff589557a0b7cabd29a442ea869c70e
whyCantIUseMyNonStuyeduAccount <nborisov@stuy.edu>
2017-04-21 08:37:43 -0400

    user input in constructor

--- Ticket.java
+++ Ticket.java
@@ -1,7 +1,20 @@
 public class Ticket implements Comparable{
     private int priority;
+    private String name;
     public Ticket(){
-	priority = 1;
+	Scanner sc = new Scanner(System.in);
+	System.out.println("what is your name?");
+	name = sc.readLine();
+	System.out.println("Is your cup holder broken?(y/n)");
+	String ans = sc.readLine();
+	if(ans = "y"){
+	    priority = 2;
+	}
+	else{
+	    System.out.println("the only other possible problem with computers is viruses");
+	    priority = 1;
+	}
+	System.out.println("Please stand by");
     }
     public int compareTo(Ticket other){
 	if( this.priority < other.priority){
@@ -10,8 +23,6 @@ public class Ticket implements Comparable{
 	else if(this.priority == other.priority){
 	    return 0;
 	}
-	else{
-		return 1;
-	}
+	else{return 1;}
     }
 }
