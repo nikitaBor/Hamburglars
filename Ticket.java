da1e2226ac7a8f5265c5ddac96ca9f6eff2a2340
whyCantIUseMyNonStuyeduAccount <nborisov@stuy.edu>
2017-04-23 14:17:45 -0400

    added compareTo method to ticket

--- Ticket.java
+++ Ticket.java
@@ -2,20 +2,24 @@ public class Ticket implements Comparable{
     private int priority;
     private String name;
     public Ticket(){
-	Scanner sc = new Scanner(System.in);
-	System.out.println("what is your name?");
-	name = sc.readLine();
-	System.out.println("Is your cup holder broken?(y/n)");
-	String ans = sc.readLine();
-	if(ans = "y"){
-	    priority = 2;
-	}
-	else{
-	    System.out.println("the only other possible problem with computers is viruses");
-	    priority = 1;
-	}
-	System.out.println("Please stand by");
+        Scanner reader = new Scanner(System.in);  // Reading from System.in
+        System.out.println("what is your name?");
+        username = reader.readLine();
+        System.out.println("do you have a problem with your cupholder(y/n)");
+        String ans = reader.readLine();
+        if(ans.equals("y")){
+            priority = 2; 
+            deskOp = "broken cupholder";
+        }
+        else{
+            System.out.println("the only other problem people have with computers are viruses.");
+            priority = 1;
+            deskOp = "viruses";
+        }
+        System.out.println("pls stand by");
     }
+
+	
     public int compareTo(Ticket other){
 	if( this.priority < other.priority){
 	    return -1;
