312d070fb8648927d140c31915eb554ba9ec11dc
yedohkang913 <ykang@stuy.edu>
2017-04-24 00:36:52 -0400

    edited, added, cleaned up

--- HelpDesk.java
+++ HelpDesk.java
@@ -1,8 +1,8 @@
-/*===
+/*======================================================
  * class HelpDesk
  * uses class Ticket and ArrayPriorityQueue
  * implementation of a help desk using a priority queue
-  ==*/
+ =====================================================*/
 
 import java.util.ArrayList;
 import java.util.Scanner;
@@ -11,114 +11,124 @@ import java.io.*;
 public class HelpDesk {
 
     private ArrayPriorityQueue<Ticket> _data;
-    private int IDtracker = 0;
+    private int IDcount = 13;
     private boolean continuous = false;
 
+    // constructor
     public HelpDesk() {
         _data = new ArrayPriorityQueue<Ticket>();
     }
 
-    public boolean dataEmpty(){
+    // checks if _data is empty
+    public boolean isDataEmpty(){
 	return _data.isEmpty();
     }
 
-    public void continuous(){
+    // checks to see if help desk should continue or not
+    public void continuous() {
 	Scanner prelim = new Scanner(System.in);
 	System.out.println("Welcome to the help desk. Do you need help? (y/n)");
 	String ans = prelim.nextLine();
 	if(ans.equals("y")){
 	    continuous = true;
 	}
-	else{
-	    continuous = false;
-	}
     }
-    
-    public void helpSequence(){
-	//	Ticket t = (Ticket)_data.removeMin();
 
-        Scanner reader = new Scanner(System.in);  // Reading from System.in     
+    // 
+    public void helpSequence(){
+	// Read from Scanner & System.in
+        Scanner reader = new Scanner(System.in);
+
+	// name input
         System.out.println("Welcome to the help desk. What is your name?");
-        //t.setName(reader.nextLine());
 	String newName = reader.nextLine();
+
+	// cupholder problem or no
         System.out.println("Do you have a problem with your cupholder?(y/n)");
-        String ans = reader.nextLine();
+        String cupAns = reader.nextLine();
+	
 	int newPriority;
 	String newDeskOp;
-        if(ans.equals("y")){
-            //t.setPriority(2);
+	
+        if (cupAns.equals("y")) {
 	    newPriority = 2;
-            //t.setDeskOp("brokenCupholder");
 	    newDeskOp = "brokenCupholder";
+	    IDcount += 1;
         }
-        else{
-            System.out.println("The only other problem people have with computers are viruses.");
-	    //t.setPriority(1);
-	    newPriority = 1;
-            //t.setDeskOp("reinstallOS");
-	    newDeskOp = "reinstallOS";
+	
+        else {
+            System.out.println("The only other problem people have with computers are viruses and passwords.");
+	    System.out.println("Do you have a problem with viruses?(y/n)");
+	    String virusAns = reader.nextLine();
+
+	    if (virusAns.equals("y")) {
+		newPriority = 0;
+		newDeskOp = "reinstallOS";
+	    }
+
+	    else {
+		System.out.println("Then you must have a problem with resetting your password.");
+		newPriority = 1;
+		newDeskOp = "passReset";
+	    }
+
+	    // increment ID
+	    IDcount += 1;
         }
+	
         System.out.println("Please stand by.");
         System.out.println("------------");
 
-	Ticket t = new Ticket(IDtracker, newPriority, newName, newDeskOp);
+	Ticket t = new Ticket(IDcount, newPriority, newName, newDeskOp);
 	_data.add(t);
     }
     
     //respond method to help user after a dequeue
     public void respond(){
+	
 	Ticket t = (Ticket)_data.removeMin();
         System.out.println("Thank you for your patience " + t.getName());
-        if(t.getDeskOp().equals("reinstallOS")){
+	
+        if (t.getDeskOp().equals("reinstallOS")) {
             System.out.println("You will need to reinstall your OS to get rid of viruses.");
         }
-        else if(t.getDeskOp().equals("brokenCupholder")){
-            System.out.println("That isn't a cupholder. It is a cd drive.");
+        else if (t.getDeskOp().equals("brokenCupholder")) {
+            System.out.println("That isn't a cupholder. It is a CD drive.");
         }
-        else{
+	else if (t.getDeskOp().equals("passReset")) {
+	    System.out.println("Please scan the page for a 'Forgot your Password?' link and follow the instructions.");
+	}
+        else {
             System.out.println("Error you have not made a request");
         }
+	
         System.out.println("We hope that we were helpful.");
         System.out.println("------------");
     }
     
-    public int setIDtracker() {
-        IDtracker += 1; 
-	return IDtracker;
-    }
-    
     public static void main(String[] args){
-	HelpDesk hello = new HelpDesk();
-	//	Ticket jeff = new Ticket(IDtracker);
-	hello.continuous();
-	while(hello.continuous){
-	    hello.helpSequence();
-	    hello.continuous();
+	
+	HelpDesk foo = new HelpDesk();
+	foo.continuous(); // 1
+ 	foo.helpSequence(); // cupholder
 
-	}
-	while(!(hello.dataEmpty())){
+	foo.continuous(); // 2
+	foo.helpSequence(); // virus
 
-	    hello.respond();
-	    hello.setIDtracker();
-	}
-	/*
-	Ticket john = new Ticket(47);
-        Scanner reader = new Scanner(System.in);  // Reading from System.in     
-        System.out.println("Welcome to the help desk. What is your name?");
-        john.name = reader.nextLine();
-        System.out.println("Do you have a problem with your cupholder?(y/n)");
-        String ans = reader.nextLine();
-        if(ans.equals("y")){
-            john.priority = 2;
-            john.deskOp = "brokenCupholder";
-        }
-        else{
-            System.out.println("The only other problem people have with computers are viruses.");
-	    john.priority = 1;
-            john.deskOp = "reinstallOS";
-        }
-        System.out.println("Please stand by.");
-        System.out.println("------------");
-	*/
+	foo.continuous(); // 3
+	foo.helpSequence(); // passreset
+
+        foo.continuous();
+	foo.helpSequence();
+
+        foo.continuous();
+	foo.helpSequence();
+	
+	foo.respond();
+	foo.respond();
+	foo.respond();
+	foo.respond();
+	foo.respond();
+  
     }
 }
--- Ticket.java
+++ Ticket.java
@@ -1,63 +1,48 @@
 /*=====
  * class Ticket
  * will be used for class HelpDesk
-  =====*/
+ =====*/
 
 import java.util.Scanner;
 
 public class Ticket implements Comparable<Ticket>{
     
-    private int priority;
-    private String name;
-    private String deskOp;
-    private int _ID;
+    private int priority; // lower number is higher priority
+    private String name; // user name
+    private String deskOp; // 
+    private int _ID; // ticket number for tracking purposes
     private boolean helped;
-    
+
+    // constructor
     public Ticket(int newID, int newPriority, String newName, String newDeskOp){
+	
         //default initializations
         _ID = -1;
         name = "UNKNOWN USER";
         priority = -1;
         deskOp = "UNKNOWN DESKOP";
         helped = false;
-        //
 	        
         _ID = newID;
 	priority = newPriority;
 	name = newName;
 	deskOp = newDeskOp;
-	/*
-        Scanner reader = new Scanner(System.in);  // Reading from System.in
-        System.out.println("Welcome to the help desk. What is your name?");
-        name = reader.nextLine();
-        System.out.println("Do you have a problem with your cupholder?(y/n)");
-        String ans = reader.nextLine();
-        if(ans.equals("y")){
-            priority = 2; 
-            deskOp = "brokenCupholder";
-        }
-        else{
-            System.out.println("The only other problem people have with computers are viruses.");
-            priority = 1;
-            deskOp = "reinstallOS";
-        }
-        System.out.println("Please stand by.");
-        System.out.println("------------");
-	*/
     }
 
-	//compareTo method for enqueueing
+    //compareTo method for enqueueing
     public int compareTo(Ticket other){
-        if( this.priority < ((Ticket)other).priority){
+        if (this.priority < ((Ticket)other).priority) {
             return -1;
         }
-        else if(this.priority == ((Ticket)other).priority){
+        else if (this.priority == ((Ticket)other).priority) {
             return 0;
         }
-        else{return 1;}
+        else {
+	    return 1;
+	}
     }
        
-    //accessors
+    // accessors + mutators
     
     public String getName(){
         return name;
@@ -95,33 +80,33 @@ public class Ticket implements Comparable<Ticket>{
     
     //main method for testing
     /* public static void main(String[] args){
-        Ticket bob = new Ticket();//answer y
-        Ticket bill = new Ticket();//answer n
-        Ticket jill = new Ticket();//answer n
-        System.out.println(bob.compareTo(bill)); //returns -1
-        System.out.println(bill.compareTo(bob));//returns 1
-        System.out.println(bill.compareTo(jill));//returns 0
-    } */
+       Ticket bob = new Ticket();//answer y
+       Ticket bill = new Ticket();//answer n
+       Ticket jill = new Ticket();//answer n
+       System.out.println(bob.compareTo(bill)); //returns -1
+       System.out.println(bill.compareTo(bob));//returns 1
+       System.out.println(bill.compareTo(jill));//returns 0
+       } */
 
     public static void main(String[] args){
 	/*
-	Ticket john = new Ticket(47);
-        Scanner reader = new Scanner(System.in);  // Reading from System.in
-        System.out.println("Welcome to the help desk. What is your name?");
-        john.name = reader.nextLine();
-        System.out.println("Do you have a problem with your cupholder?(y/n)");
-        String ans = reader.nextLine();
-        if(ans.equals("y")){
-            john.priority = 2; 
-            john.deskOp = "brokenCupholder";
-        }
-        else{
-            System.out.println("The only other problem people have with computers are viruses.");
-            john.priority = 1;
-            john.deskOp = "reinstallOS";
-        }
-        System.out.println("Please stand by.");
-        System.out.println("------------");
+	  Ticket john = new Ticket(47);
+	  Scanner reader = new Scanner(System.in);  // Reading from System.in
+	  System.out.println("Welcome to the help desk. What is your name?");
+	  john.name = reader.nextLine();
+	  System.out.println("Do you have a problem with your cupholder?(y/n)");
+	  String ans = reader.nextLine();
+	  if(ans.equals("y")){
+	  john.priority = 2; 
+	  john.deskOp = "brokenCupholder";
+	  }
+	  else{
+	  System.out.println("The only other problem people have with computers are viruses.");
+	  john.priority = 1;
+	  john.deskOp = "reinstallOS";
+	  }
+	  System.out.println("Please stand by.");
+	  System.out.println("------------");
 	*/
     }
 }
